ad39823e7a39ba18e9058c43f8fb92c90cb2da47
Shekhar <151825448+MyNameIsShekhar@users.noreply.github.com>
2024-12-21 19:42:02 +0530

    Update links.js

--- src/data/links.js
+++ src/data/links.js
@@ -1,6 +1,6 @@
 export const links = [
   { path: "/", label: "Home" },
-  { path: "/About", label: "About us" },
+  { path: "/About", label: "About" },
   { path: "/Services", label: "Services" },
   { path: "/Project", label: "Contact" },
   ];
