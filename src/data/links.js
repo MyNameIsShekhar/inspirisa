3dba43474716d067bfa26ab2f4d9e5edde27bcfe
Shekhar <151825448+MyNameIsShekhar@users.noreply.github.com>
2024-12-21 19:34:03 +0530

    Update links.js

--- src/data/links.js
+++ src/data/links.js
@@ -1,9 +1,6 @@
 export const links = [
   { path: "/", label: "Home" },
-  { path: "/temp", label: "COURSES" },
-  { path: "/temp", label: "CERTIFICATIONS" },
-  { path: "/temp", label: "GLOBAL COMMUNITY" },
-  { path: "/temp", label: "BUSINESS EXCURSIONS" },
-  { path: "/temp", label: "SAUDI VISION 2030" },
-  { path: "/temp", label: "BUSINESS PARTNERS" },
-];
+  { path: "/About", label: "About us" },
+  { path: "/Services", label: "Services" },
+  { path: "/Project", label: "Contact" },
+  ];
